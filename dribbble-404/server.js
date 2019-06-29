a964392ee485f28115f374e9cd907246218376d2
wanlinlin <leo12345639@gmail.com>
2019-06-29 21:01:49 +0800

    Update prod url

--- dribbble-404/server.js
+++ dribbble-404/server.js
@@ -3,6 +3,6 @@ const express = require('express')
 const app = express()
 
 app.use(express.static('public'))
-app.use('/mimik', express.static('public'))
+app.use('/mimik/dribbble-404', express.static('public'))
 
 app.listen(5000)
